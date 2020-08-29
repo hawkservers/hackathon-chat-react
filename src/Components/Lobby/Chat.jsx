1e99b799b3d43c1b01e529a4f0dc6c0be2baceb6
REAZN <reasonwhygamer@gmail.com>
2020-08-29 20:10:43 +0100

    style

--- src/Components/Lobby/Chat.jsx
+++ src/Components/Lobby/Chat.jsx
@@ -1,10 +1,148 @@
 import React, {Component} from 'react';
+import "./chat.scss"
 
 export default class Chat extends Component {
   render() {
     return (
-      <div>
-        big fat wang chat
+      <div className="chat">
+        <div className="messageList">
+          <div className="messageContainer">
+              <span className="name">Lewis</span>
+              <div className="splitter">
+                <img className="image"/>
+                <div className="message">
+                    This is super cool!
+                </div>
+            </div>
+          </div>
+          <div className="messageContainer">
+              <span className="name">George</span>
+              <div className="splitter">
+                <img className="image"/>
+                <div className="message">
+                    This is super cool!
+                </div>
+            </div>
+          </div>
+          <div className="messageContainer">
+              <span className="name">Zeo</span>
+              <div className="splitter">
+                <img className="image"/>
+                <div className="message">
+                    This is super cool!
+                </div>
+            </div>
+          </div>
+          <div className="messageContainer">
+              <span className="name">Mason</span>
+              <div className="splitter">
+                <img className="image"/>
+                <div className="message">
+                    This is super cool! sh dkshdjkhsa kjdha sdjsah djkashd kjhasdkj haskjdh akjhd haskjdhakjhd kashdhad haskjd haskjhdkjahsdjkhasd hajhd ajhs dhakdha sjhda d
+                </div>
+            </div>
+          </div>
+          <div className="messageContainer">
+              <span className="name">Your mum</span>
+              <div className="splitter">
+                <img className="image"/>
+                <div className="message">
+                    This is super cool!
+                </div>
+            </div>
+          </div>
+          <div className="messageContainer">
+              <span className="name">Your mum</span>
+              <div className="splitter">
+                <img className="image"/>
+                <div className="message">
+                    This is super cool!
+                </div>
+            </div>
+          </div>
+          <div className="messageContainer">
+              <span className="name">Your mum</span>
+              <div className="splitter">
+                <img className="image"/>
+                <div className="message">
+                    This is super cool!
+                </div>
+            </div>
+          </div>
+          <div className="messageContainer">
+              <span className="name">Your mum</span>
+              <div className="splitter">
+                <img className="image"/>
+                <div className="message">
+                    This is super cool!
+                </div>
+            </div>
+          </div>
+          <div className="messageContainer">
+              <span className="name">Your mum</span>
+              <div className="splitter">
+                <img className="image"/>
+                <div className="message">
+                    This is super cool!
+                </div>
+            </div>
+          </div>
+          <div className="messageContainer">
+              <span className="name">Your mum</span>
+              <div className="splitter">
+                <img className="image"/>
+                <div className="message">
+                    This is super cool!
+                </div>
+            </div>
+          </div>
+          <div className="messageContainer">
+              <span className="name">Your mum</span>
+              <div className="splitter">
+                <img className="image"/>
+                <div className="message">
+                    This is super cool!
+                </div>
+            </div>
+          </div>
+          <div className="messageContainer">
+              <span className="name">Your mum</span>
+              <div className="splitter">
+                <img className="image"/>
+                <div className="message">
+                    This is super cool!
+                </div>
+            </div>
+          </div>
+          <div className="messageContainer">
+              <span className="name">Your mum</span>
+              <div className="splitter">
+                <img className="image"/>
+                <div className="message">
+                    This is super cool!
+                </div>
+            </div>
+          </div>
+          <div className="messageContainer">
+              <span className="name">Lewis</span>
+              <div className="splitter">
+                <img className="image"/>
+                <div className="message">
+                    This is super cool! kdhkhdjhfdk hfdkjh kjfshdjfhksjd hfkjdh fhskjdhf jksh dfshfk
+                </div>
+            </div>
+          </div>
+          <div className="messageContainer">
+              <span className="name">Lewis</span>
+              <div className="splitter">
+                <img className="image"/>
+                <div className="message">
+                    This is super cool! kdhkhdjhfdk hfdkjh kjfshdjfhksjd hfkjdh fhskjdhf jksh dfshfk
+                </div>
+            </div>
+          </div>
+        </div>
+        <input placeholder="Send a message"/>
       </div>
     )
   }
--- src/Pages/Lobby.jsx
+++ src/Pages/Lobby.jsx
@@ -1,13 +1,23 @@
 import React, {Component} from 'react';
 import Chat from "../Components/Lobby/Chat";
 
+import "./lobby.scss"
+
 export default class Lobby extends Component {
   lobbyId = this.props.match.params.lobby;
 
   render() {
     return (
-      <div>
+      <div className="lobby">
+        <span className="title">
         {this.lobbyId}
+        </span>
+        <div className="buttons">
+            <div className="button"/>
+            <div className="button"/>
+            <div className="button"/>
+            <div className="button"/>
+        </div>
         <Chat/>
       </div>
     )
