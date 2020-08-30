3efa3a26155841ddbfa13b349185f5e8b8f99ee6
Mex de Loo <mex@zeodev.cc>
2020-08-30 10:42:23 +0200

    fontaweasome

--- src/Pages/Lobby.jsx
+++ src/Pages/Lobby.jsx
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
 import Chat from "../Components/Lobby/Chat";
 import Cams from "../Components/Lobby/Cams";
-
 import "./lobby.scss";
 import Socket from "../Services/Socket";
+import {faMicrophone, faPhoneSlash, faRandom, faVideo} from "@fortawesome/pro-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 export default class Lobby extends Component {
   lobbyId = this.props.match.params.lobby;
@@ -18,10 +19,18 @@ export default class Lobby extends Component {
         <span className="title">{this.lobbyId}</span>
           <Cams/>
         <div className="buttons">
-            <div className="button"/>
-            <div className="button"/>
-            <div className="button"/>
-            <div className="button"/>
+            <button className="button">
+              <FontAwesomeIcon icon={faMicrophone}/>
+            </button>
+            <button className="button">
+              <FontAwesomeIcon icon={faVideo}/>
+            </button>
+            <button className="button">
+              <FontAwesomeIcon icon={faPhoneSlash}/>
+            </button>
+            <button type="button" className="button" onClick={() => Socket.joinRandom()}>
+              <FontAwesomeIcon icon={faRandom}/>
+            </button>
         </div>
         <Chat/>
       </div>
